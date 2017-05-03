653a13977e88fa35fe2eaacc2e65e664067a3b6a
elliesterner13@gmail.com <elliesterner@Ellies-MacBook-Pro.local>
2017-05-03 16:57:49 -0500

    made some changes with manifest

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -1,6 +1,23 @@
+//const axios = require('axios')
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
-  if(tab.url.indexOf('en.wikipedia.org') > -1){
+  if(tab.url.indexOf('wikipedia.org') > -1){
     console.log('worked')
     chrome.pageAction.show(tab.id)
   }
+})
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.type==='sendTitle') {
+		console.log(request.data)
+		jQuery.ajax('/api/users/3', {
+			method: 'GET',
+			success: function(user) {
+				console.log('user', user)
+			},
+			error: function() {
+				console.log('error')
+			}
+		})
+
+	}
 })
