8c19dc5dca4c731319e03fdf483517ac57720695
Nick Lee <nicklee100@gmail.com>
2017-05-05 12:34:25 -0500

    started oauth, set up app id, not working

--- chromeExtension/js/background.js
+++ chromeExtension/js/background.js
@@ -2,6 +2,38 @@
 	- create new History array for user doesn't work yet
 ********/
 
+/******  OAUTH *******/
+
+// var oauth = ChromeExOAuth.initBackgroundPage({
+//   'request_url': 'https://www.google.com/accounts/OAuthGetRequestToken',
+//   'authorize_url': 'https://www.google.com/accounts/OAuthAuthorizeToken',
+//   'access_url': 'https://www.google.com/accounts/OAuthGetAccessToken',
+//   'consumer_key': 'anonymous',
+//   'consumer_secret': 'anonymous',
+//   'scope': 'https://docs.google.com/auth/drive',
+//   'app_name': 'WikiWebJr'
+// });
+
+
+
+// function callback(resp, xhr) {
+// 	console.log('in callback reponse', resp)
+// }
+
+// function onAuthorized() {
+//   var url = 'https://docs.google.com/feeds/default/private/full';
+//   var request = {
+//     'method': 'GET',
+//     'parameters': {'alt': 'json'}
+//   }
+
+//   // Send: GET https://docs.google.com/feeds/default/private/full?alt=json
+//   oauth.sendSignedRequest(url, callback, request);
+// }
+
+// oauth.authorize(onAuthorized);
+
+
 
 
 /* ******* STORE ********/
@@ -45,7 +77,7 @@ const postHistory = function(userId) {
 const postLink = function() {
 	let linkData = {
 	  	source: store.previousNode,
-	  	target: store.currentNode, 
+	  	target: store.currentNode,
 	  	isHyperText: true,
 	  	userId: 1
 	 }
@@ -99,10 +131,10 @@ const fetchHistoryData = function(historyInfo) {
 
 const fetchLinkData = function(linkInfo) {
   return fetch('http://localhost:8000/api/links', {
-    method: 'POST', 
+    method: 'POST',
     headers: {
     "Content-type": "application/json"
-	}, 
+	},
 	body: JSON.stringify(linkInfo)
 })
 
@@ -132,7 +164,11 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 					sendResponse(user)
 				})
 				return true
-				
+				case 'START_AUTH':
+					 chrome.identity.getAuthToken({"interactive": true}, function(token){
+    				console.log('this is token', token)
+  				})
+					break;
 
 			default:
 				return console.error('error in switch')
--- chromeExtension/js/popup.js
+++ chromeExtension/js/popup.js
@@ -1,7 +1,13 @@
 const dashButton = document.getElementById('toDash')
+const authButton = document.getElementById('auth')
 
 dashButton.addEventListener('click', function(evt) {
   chrome.tabs.create({
     url: chrome.runtime.getURL('views/dashboard.html')
   })
+})
+
+authButton.addEventListener('click', function(evt){
+  chrome.runtime.sendMessage({'type': "START_AUTH"})
+
 })
