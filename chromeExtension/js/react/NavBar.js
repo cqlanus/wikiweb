693b2ce1d430a70f6eb0f3d75406744c065c0c6b
Christopher Lanus <cqlanus@gmail.com>
2017-05-07 14:27:31 -0500

    Webpack, React, React-router (#22)
    * working on ui
    
    * webpack up and running, react working, reorganized directory structure
    
    * react-router working
    
    * added gitignore
    
    * working on d3 changes
    
    * bundle
    
    * fixed untracked files

---
+++ chromeExtension/js/react/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+const NavBar = props => (
+  <nav className="navigation">
+    <div id="title">My Dashboard</div>
+    <ul>
+      <li className="btn"><Link to="/">WikiWeb</Link></li>
+      <li className="btn"><Link to="/history">WikiHistory</Link></li>
+      <li className="btn"><Link href="#">WikiAnalysis</Link></li>
+    </ul>
+  </nav>
+)
+
+export default NavBar
