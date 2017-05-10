c1430543deb239fd9adc555ae0eff46474216abc
Chris Lanus <cqlanus@gmail.com>
2017-05-10 16:15:28 -0500

    historyzoomer working

---
+++ chromeExtension/js/d3/historyzoomer.js
@@ -0,0 +1,37 @@
+const d3 = require('d3')
+
+function d3Zoom(history, idx) {
+
+    const d3NodeArr = d3.selectAll('circle').empty() ? 'empty' : d3.selectAll('circle').nodes()
+    const nodeDataArr = d3.selectAll('circle').empty() ? 'empty' : d3.selectAll('circle').nodes().map(node => node.__data__)
+    const currentNodeData = nodeDataArr.find(node => {
+      return node.id === history[idx]
+    })
+
+    const currentD3Node = d3NodeArr.find(node => node.__data__.id === currentNodeData.id)
+
+    d3.selectAll('circle').classed('selected', false)
+    d3.select(currentD3Node).attr('class', 'selected')
+
+    const xOffset = currentNodeData.x,
+          yOffset = currentNodeData.y
+
+    const zoom = d3.zoom().on('zoom', zoomer)
+
+    function zoomer() {
+      const scaleMultipler = 4,
+            height = d3.selectAll('.rect').node().getBBox().height,
+            width = d3.selectAll('.rect').node().getBBox().width,
+            center = [width/2, height/2],
+            xTranlation = center[0] - xOffset * scaleMultipler,
+            yTranslation = center[1]-yOffset * scaleMultipler
+
+      drawSpace.transition().duration(1000)
+      .attr('transform', `translate(${xTranlation}, ${yTranslation})scale(${scaleMultipler})`)
+    }
+
+    const drawSpace = d3.selectAll('.draw')
+    drawSpace.call(zoom.transform, d3.zoomIdentity)
+}
+
+module.exports = d3Zoom
--- chromeExtension/js/react/ForceChart.js
+++ chromeExtension/js/react/ForceChart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import createForceChart from '../d3/dashboard.js'
 const d3 = require('d3')
+const d3Zoom = require('../d3/historyzoomer')
 
 class ForceChart extends React.Component {
   constructor() {
@@ -23,12 +24,12 @@ class ForceChart extends React.Component {
 
   getHistory() {
     chrome.runtime.sendMessage({
-    type: 'getUser',
-    data: '115893302668387505418'
-  }, (results) => {
-      this.setState({
-        pageHistory: results.history.history
-      })
+      type: 'getUser',
+      data: '115893302668387505418'
+    }, (results) => {
+        this.setState({
+          pageHistory: results.history.history
+        })
     })
   }
 
@@ -36,61 +37,32 @@ class ForceChart extends React.Component {
     console.log('toggle')
     this.setState({
       historyView: !this.state.historyView,
-      currentNodeId: this.state.currentNodeId + 1
+      currentNodeId: 0
     })
 
-  this.zoomFn()
-  }
-
-  zoomFn() {
-
-    const d3NodeArr = d3.selectAll('circle').empty() ? 'empty' : d3.selectAll('circle').nodes()
-    const nodeDataArr = d3.selectAll('circle').empty() ? 'empty' : d3.selectAll('circle').nodes().map(node => node.__data__)
-    const currentNodeData = nodeDataArr.find(node => {
-      return node.id === this.state.pageHistory[this.state.currentNodeId]
-    })
-    // console.log('d3NodeArr', d3NodeArr)
-    const currentD3Node = d3NodeArr.find(node => node.__data__.id === currentNodeData.id)
-    console.log('currentNodeData', currentNodeData, currentD3Node)
-    d3.select(currentD3Node).attr('class', 'selected')
-
-    const xOffset = currentNodeData.x,
-          yOffset = currentNodeData.y
-
-    const zoom = d3.zoom().on('zoom', zoomer)
-
-    function zoomer() {
-      const scaleMultipler = 4,
-            height = d3.selectAll('.rect').node().getBBox().height,
-            width = d3.selectAll('.rect').node().getBBox().width,
-            center = [width/2, height/2],
-            xTranlation = center[0] - xOffset * scaleMultipler,
-            yTranslation = center[1]-yOffset * scaleMultipler
-
-      drawSpace.transition().duration(1000)
-      .attr('transform', `translate(${xTranlation}, ${yTranslation})scale(${scaleMultipler})`)
-    }
-
-    const drawSpace = d3.selectAll('.draw')
-    drawSpace.call(zoom.transform, d3.zoomIdentity)
   }
 
   handleNextNode(evt) {
     this.setState({
       currentNodeId: this.state.currentNodeId + 1
     })
-    this.zoomFn()
+
   }
 
   handlePrevNode(evt) {
     this.setState({
       currentNodeId: this.state.currentNodeId - 1
     })
-    this.zoomFn()
+
+  }
+
+  zoomFn() {
+    d3Zoom(this.state.pageHistory, this.state.currentNodeId)
   }
 
   render() {
-    console.log('currentNodeId & history position', this.state.currentNodeId, this.state.pageHistory[this.state.currentNodeId])
+    this.state.pageHistory.length && this.state.historyView ? this.zoomFn() : null
+
   return (
   <div className="canvas-container">
     <svg height="700" width="100%"></svg>
