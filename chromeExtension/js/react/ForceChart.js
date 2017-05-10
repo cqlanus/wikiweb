34cda7531aeea5039fbb363a3681e56ca28a2650
Chris Lanus <cqlanus@gmail.com>
2017-05-10 12:31:01 -0500

    can marginally zoom to selected node

--- chromeExtension/js/react/ForceChart.js
+++ chromeExtension/js/react/ForceChart.js
@@ -16,17 +16,33 @@ class ForceChart extends React.Component {
   }
 
   handleToggle(evt) {
+    console.log('toggle')
     this.setState({
       history: !this.state.history
     })
 
+  this.zoomFn()
+
+  }
+
+  zoomFn() {
+    const selectedCircle = d3.selectAll('.selected')
+    const circleData = selectedCircle._groups[0][0] ? selectedCircle._groups[0][0].__data__ : {x: 250, y: 110}
+    let x = circleData.x
+    let y = circleData.y
     const zoom = d3.zoom().on('zoom', zoomed)
 
-    // const circleArray = [...d3.select('svg').selectAll('circle')._groups[0]]
-    // const firstCircle = circleArray.filter(circle => circle.__data__.id === 1)[0]
-    const firstCircle = d3.selectAll('circle')
+    function zoomed() {
+      const k = 500 / 180
+      const center = [540/2, 500/2]
+      console.log('zoomed called')
+      drawSpace.transition().duration(1000)
+      .attr('transform', `translate(${center[0]-x * k}, ${center[1]-y * k})scale(3)`)
+    }
+    const drawSpace = d3.selectAll('.draw')
 
 
+    drawSpace.call(zoom.transform, d3.zoomIdentity)
   }
 
   handleNextNode(evt) {
@@ -42,6 +58,8 @@ class ForceChart extends React.Component {
   }
 
   render() {
+
+    this.zoomFn()
   return (
   <div className="canvas-container">
     <svg height="700" width="100%"></svg>
