1d2a9c6a3f0f567bea00f8c7e4ae94f8febd9ddb
Nick Lee <nicklee100@gmail.com>
2017-05-14 11:19:49 -0500

    removed hard coded data

--- chromeExtension/js/react/UserModal.js
+++ chromeExtension/js/react/UserModal.js
@@ -4,38 +4,10 @@ const d3 = require('d3')
 class UserModal extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      viewtodisplay: null,
-      ACategories: {
-      'Law': 2,
-      'Culture': 8,
-      'Movies': 4,
-      'Science': 2,
-      'People': 1,
-      'Actors': 1,
-      'Beer': 2,
-      'Government': 7,
-      'Education': 4,
-      'Politics': 9,
-      'Religion': 12,
-      'Weather': 2 ,
-      'Real Estate': 1
-    },
-      pageHistory: [2,3,5,],
-      totalArticles: 18,
-      totalPageVisits: 30,
-    }
+    this.state = { }
    this.CalculateTopCategories = this.CalculateTopCategories.bind(this)
    this.SortCat = this.SortCat.bind(this)
   }
-
-
-  componentWillMount() {
-    console.log('props in did mount',this.props.cat)
-   this.setState({
-     ACategories: this.props.cat
-   })
-   }
 
   ObjtoArr(cat){
     return Object.keys(cat).map(key=>{
@@ -67,7 +39,7 @@ class UserModal extends React.Component {
        <table className="tableHeader">
          <tbody>
             <tr className="descRow">
-              <th>Top Catagories</th>
+              <th>Top 5 Catagories</th>
               <th>Number of Visits</th>
             </tr>
               {topFive.map(data => {
