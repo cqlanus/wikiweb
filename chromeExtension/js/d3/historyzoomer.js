689a832d65078fc88589123db571d285dd3415c4
Christopher Lanus <cqlanus@gmail.com>
2017-05-15 17:07:49 -0500

    WikiAnalysis legend added (#46)
    * scatter view brushes x and y axes
    
    * color coded dots by category
    
    * commit before merge
    
    * sentiment map varies color by confidence
    
    * prep for merge
    
    * small commit
    
    * wikiweb history view has titles and images
    
    * legend added to wikianalysis

--- chromeExtension/js/d3/historyzoomer.js
+++ chromeExtension/js/d3/historyzoomer.js
@@ -8,7 +8,7 @@ function d3Zoom(history, idx, historyView) {
       return node.id === history[idx]
     })
 
-    console.log('currentNodeData', currentNodeData)
+    // console.log('currentNodeData', currentNodeData)
 
     const currentD3Node = d3NodeArr.find(node => node.__data__.id === currentNodeData.id)
 
@@ -19,39 +19,66 @@ function d3Zoom(history, idx, historyView) {
           yOffset = currentNodeData.y
 
     const zoom = d3.zoom().on('zoom', zoomer)
+    const drawSpace = d3.selectAll('.draw')
+    drawSpace.call(zoom.transform)
 
     function zoomer() {
       const scaleMultipler = 4,
             height = d3.selectAll('.rect').node().getBBox().height,
             width = d3.selectAll('.rect').node().getBBox().width,
             center = [width/2, height/2],
-            xTranslation = center[0] - xOffset * scaleMultipler,
-            yTranslation = center[1]-yOffset * scaleMultipler
+            xTranslation = center[0] - xOffset,
+            yTranslation = center[1]-yOffset
 
       drawSpace.transition().duration(500)
-      .attr('transform', `translate(${xTranslation}, ${yTranslation})scale(${scaleMultipler})`)
-    // createToolTip(currentNodeData, xTranslation, yTranslation)
+      .attr('transform', `translate(${xTranslation}, ${yTranslation})`)
+
+      createToolTip(currentNodeData, 0, 0, idx)
     }
-    const drawSpace = d3.selectAll('.draw')
-    drawSpace.call(zoom.transform, d3.zoomIdentity)
 }
 
-function createToolTip(node, xTrans, yTrans) {
+function createToolTip(node, xTrans, yTrans, index) {
  /* CREATE HOVERABLE TOOLTIPS */
-  const divTooltip = d3.select("rect")
-      .append("text")
-      .attr("class", "toolTip")
+ d3.selectAll('.pageInfo').remove()
+
+  const divG = d3.select(".g-main")
+      .append('g')
+      .attr("class", "pageInfo")
+
+  divG.append('rect')
       .attr("x", 20 + xTrans)
       .attr("y", 20 + yTrans)
-      .attr('width', 100)
-      .attr('height', 60)
-      .text(d => node.title)
+      .attr('width', 400)
+      .attr('height', 100)
+      .attr('fill', 'black')
+      .attr('opacity', 0.7)
 
-      divTooltip.style("display", "inline-block");
-      divTooltip.html(`
-        ${node.title} <br>
-        ${node.category}
-      `);
+  const text = d3.select('.g-main')
+    .append("text")
+    .attr('class', 'pageInfo')
+    .attr('transform', `translate(${30}, ${40})`)
+
+    text.append('tspan')
+      .attr('x', 0)
+      .attr('dy', '0.6em')
+      .style('font-size', '1.5em')
+      .attr('fill', 'white')
+      .text(d => {
+        return `${index+1}. ${formatTitle(node.title)}`
+      })
+
+    divG.append('image')
+      .attr('class', 'pageInfo')
+      .attr('href', d => node.pictureUrl)
+      .attr('x', 300)
+      .attr('y', 20 + yTrans)
+      .attr('height', '100px')
+      .attr('width', '100px')
+}
+
+function formatTitle(title) {
+  const idx = title.indexOf(' - Wiki')
+  return title.substring(0, idx)
 }
 
 module.exports = {d3Zoom,}
