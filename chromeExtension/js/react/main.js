499530df79943d7814f170da35e551384681f157
Ellie Sterner <elliesterner13@gmail.com>
2017-05-16 16:40:26 -0500

    All analysis es (#57)
    * got sentiment analysis all to work
    
    * wikiscatter working
    
    * finna cool it

--- chromeExtension/js/react/main.js
+++ chromeExtension/js/react/main.js
@@ -44,6 +44,14 @@ const onScatterEnter = () => {
   })
 }
 
+const onAllScatterEnter = () => {
+  chrome.runtime.sendMessage({
+    type: 'GET_NODEIDS_BY_USER'
+  }, selectedNodes => {
+    createNodeScatter(selectedNodes)
+  })
+}
+
 const onSentimentEnter = () => {
   chrome.runtime.sendMessage({
     type: 'GET_SELECTED',
@@ -57,6 +65,22 @@ const onSentimentEnter = () => {
   })
 }
 
+const onAllSentimentEnter = () => {
+  chrome.runtime.sendMessage({
+    type: 'GET_NODEIDS_BY_USER',
+  }, selectedNodes => {
+    console.log('selectedNodes in on enter', selectedNodes)
+    chrome.runtime.sendMessage({
+      type: 'GET_SENTIMENT_BY_USERID',
+      data: selectedNodes,
+    }, analysis => {
+      createSentimentMap(analysis)
+    })
+  })
+}
+
+
+
 
 
 ReactDOM.render(
@@ -64,8 +88,10 @@ ReactDOM.render(
     <Route path='/' component={Main}>
       <IndexRedirect to='/web' />
       <Route path='/web' component={ForceChart} onEnter={onWebEnter}/>
+      <Route path='/allhistory' component={History} onEnter={onAllScatterEnter}/>
       <Route path='/history' component={History} onEnter={onScatterEnter}/>
       <Route path='/sentiment' component={Sentiment} onEnter={onSentimentEnter}/>
+      <Route path='/allsentiment' component={Sentiment} onEnter={onAllSentimentEnter}/>
     </Route>
   </Router>,
   document.getElementById('app'))
