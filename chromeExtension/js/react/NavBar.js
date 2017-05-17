f1741ab41d25de30391d6c159ae9a9dd81e877e6
Nick Lee <nicklee100@gmail.com>
2017-05-17 12:05:26 -0500

    tweaks

--- chromeExtension/js/react/NavBar.js
+++ chromeExtension/js/react/NavBar.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router'
 
 const NavBar = props => (
   <nav className="navigation">
-    <div id="title">My Dashboard</div>
+    {/*<div id="title">My Dashboard</div>*/}
+                <img id='title' src="../../assets/logo-wide.jpeg"/>
+
     <ul>
       <li className="btn"><Link to="/">WikiWeb</Link></li>
       <li className="btn"><Link to="/allhistory">WikiHistory</Link></li>
